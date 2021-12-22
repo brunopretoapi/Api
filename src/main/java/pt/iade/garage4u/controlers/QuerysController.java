d9378828db87e2f9dda7e96f8f681fca0a0d0651
brunopreto <garage4uiade@gmail.com>
2021-12-22 19:43:39 +0000

    querys

--- src/main/java/pt/iade/garage4u/controlers/QuerysController.java
+++ src/main/java/pt/iade/garage4u/controlers/QuerysController.java
@@ -3,6 +3,7 @@ package pt.iade.garage4u.controlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -41,6 +42,47 @@ public class QuerysController {
         return queryRepository.garagens_info(id);
     }
 
+    @GetMapping(path = "/controlo_porta/{nome:[.-z]+}/{pass:[.-z]+}/{cod_dispositivo:[a-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> get_controlo_porta(@PathVariable("nome") String nome,@PathVariable("pass") String pass,@PathVariable("cod_dispositivo") String cod_dispositivo) {
+        logger.info("Sending bio from route nome:,pass:,cod_dispositivo:" + nome +pass + cod_dispositivo);
+        return queryRepository.controlo_porta(nome,pass,cod_dispositivo);
+    }
+
+    @GetMapping(path = "/controlo_lampada/{nome:[.-z]+}/{pass:[.-z]+}/{cod_dispositivo:[a-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> get_controlo_lampada(@PathVariable("nome") String nome,@PathVariable("pass") String pass,@PathVariable("cod_dispositivo") String cod_dispositivo) {
+        logger.info("Sending bio from route nome:,pass:,cod_dispositivo:" + nome +pass + cod_dispositivo);
+        return queryRepository.controlo_lampada(nome,pass,cod_dispositivo);
+    }
+
+    @GetMapping(path = "/controlo_tem_lampada/{cod_dispositivo:[a-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> get_controlo_tem_lampada(@PathVariable("cod_dispositivo") String cod_dispositivo) {
+        logger.info("Sending bio from route nome:,pass:,cod_dispositivo:" + cod_dispositivo);
+        return queryRepository.controlo_tem_lampada(cod_dispositivo);
+    }
+
+    @GetMapping(path = "/estado_portao/{cod_dispositivo:[a-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> get_estado_portao(@PathVariable("cod_dispositivo") String cod_dispositivo) {
+        logger.info("Sending bio from route nome:,pass:,cod_dispositivo:" + cod_dispositivo);
+        return queryRepository.estado_portao(cod_dispositivo);
+    }
+
+    @GetMapping(path = "/estado_luz/{cod_dispositivo:[a-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> get_estado_luz(@PathVariable("cod_dispositivo") String cod_dispositivo) {
+        logger.info("Sending bio from route nome:,pass:,cod_dispositivo:" + cod_dispositivo);
+        return queryRepository.estado_luz(cod_dispositivo);
+    }
+
+
+
+
+    /*
+
+    @GetMapping(path = "/utilizador_reservas/{nome:[.-z]+}/{pass:[.-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> getutilizador_reservas(@PathVariable("nome") String nome,@PathVariable("pass") String pass) {
+        logger.info("Sending bio from route nome: ,pass:" + nome +pass);
+        return queryRepository.utilizador_reserva(nome,pass);
+    }
+
     @GetMapping(path = "/teste/{nome:[A-z]+}/{morada:[.-z]+}/{genero:[a-z]+}/{data:[--9]+}/{identificacao:[0-9]+}/{email:[.-z]+}/{pass:[.-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<String> get_teste(@PathVariable("nome") String nome,@PathVariable("morada") String morada,@PathVariable("genero") String genero,@PathVariable("data") Date data,@PathVariable("identificacao") int identificacao,@PathVariable("email") String email,@PathVariable("pass") String pass) {
         logger.info("Sending bio from route nome:,morada:,genero:,data:,identificacao:,email:,pass:" + nome + morada + genero + data + identificacao+email+pass);
@@ -55,18 +97,11 @@ public class QuerysController {
 
 
 
-    /*
     // terminar
    @GetMapping(path = "/cria_utilizador/nome:[A-z]+}/morada:[.-z]+}/genero:[A-Z]+}/data:[.-z]+}/identificacao:[0-9]+}/email:[.-z]+}/pass:[.-z]+}/num_card:[0-9]+}/cvv:[0-9]+}/nome_cartao:[A-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<String> get_cria_utilizador(@PathVariable("nome") String nome,@PathVariable("morada") String morada,@PathVariable("genero") Character genero,@PathVariable("data") String data,@PathVariable("identificacao") int identificacao,@PathVariable("email") String email,@PathVariable("pass") String pass,@PathVariable("num_card") int num_card,@PathVariable("cvv") int cvv,@PathVariable("nome_cartao") String nome_cartao) {
         logger.info("Sending bio from route nome:,morada:,genero:,data:,identificacao:,email:,pass:, num_card:, cvv: ,nome_cartao:" + nome+morada+genero+data+identificacao+email+pass+num_card+cvv+nome_cartao);
         return queryRepository.criar_utilizador(nome,morada,genero,data,identificacao,email,pass,num_card,cvv,nome_cartao);
-    }
-
-    @GetMapping(path = "/utilizador_reservas/{nome:[.-z]+}/{pass:[.-z]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<String> getutilizador_reservas(@PathVariable("nome") String nome,@PathVariable("pass") String pass) {
-        logger.info("Sending bio from route nome: ,pass:" + nome +pass);
-        return queryRepository.utilizador_reserva(nome,pass);
     }
 
     @GetMapping(path = "/adicionar_garagens/{utilizador_email:[.-z]+}/{utilizador_pass:[.-z]+}/{garagem_localizacao:[0-z]+}/{garagem_zona:[a-z]+}/{garagem_zona_cod:[0-9]+}/{garagem_lampada:[0-1]+}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -79,6 +114,13 @@ public class QuerysController {
 
 
     // banco
+
+    @GetMapping(path = "/banco/confirma_trasacao/{utilizador:[A-z]+}/{num_card:[0-9]+}/{cvv:[0-9]+}/{quantidade_dinheiro:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<String> getconfirma_trasacao(@PathVariable("utilizador") String utilizador,@PathVariable("num_card") int num_card,@PathVariable("cvv") int cvv,@PathVariable("quantidade_dinheiro") int quantidade_dinheiro) {
+        logger.info("Sending bio from route utilizador: ,num_card:,cvv:,quantidade_dinheiro:" + utilizador +num_card+cvv+quantidade_dinheiro);
+        return queryRepository.confirma_trasacao(utilizador ,num_card,cvv,quantidade_dinheiro);
+    }
+
     @GetMapping(path = "/banco/depositar/{utilizador:[A-z]+}/{num_card:[0-9]+}/{cvv:[0-9]+}/{quantidade_dinheiro:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<String> getdepositar(@PathVariable("utilizador") String utilizador,@PathVariable("num_card") int num_card,@PathVariable("cvv") int cvv,@PathVariable("quantidade_dinheiro") int quantidade_dinheiro) {
         logger.info("Sending bio from route utilizador: ,num_card:,cvv:,quantidade_dinheiro:" + utilizador +num_card+cvv+quantidade_dinheiro);
--- src/main/java/pt/iade/garage4u/repository/QueryRepository.java
+++ src/main/java/pt/iade/garage4u/repository/QueryRepository.java
@@ -19,9 +19,8 @@ public interface QueryRepository extends CrudRepository<Utilizador, Integer> {
     @Query(value = "select levantar(:utilizador, :num_card, :cvv, :quantidade_dinheiro)", nativeQuery = true)
     Iterable<String> levanta(String utilizador, int num_card, int cvv, int quantidade_dinheiro);
 
-    // terminar
-    @Query(value = "select utilizador_reservas(:nome,:pass)", nativeQuery = true)
-    Iterable<String> utilizador_reserva(String nome, String pass);
+    @Query(value = "select confirma_trasacao(:utilizador, :num_card, :cvv, :quantidade_dinheiro)", nativeQuery = true)
+    Iterable<String> confirma_trasacao(String utilizador, int num_card, int cvv, int quantidade_dinheiro);
 
     @Query(value = "select * from utilizador where utilizador_email = :nome and utilizador_pass = :pass", nativeQuery = true)
     Iterable<Utilizador> utilizador_info(String nome, String pass);
@@ -29,14 +28,35 @@ public interface QueryRepository extends CrudRepository<Utilizador, Integer> {
     @Query(value = "select * from garagens where garagens_id = :id", nativeQuery = true)
     Iterable<String> garagens_info(int id);
 
+    @Query(value = " select dispositivo_porta(:nome,:pass,:cod_dip)", nativeQuery = true)
+    Iterable<String> controlo_porta(String nome, String pass, String cod_dip);
+
+    @Query(value = " select dispositivo_lampada(:nome,:pass,:cod_dip)", nativeQuery = true)
+    Iterable<String> controlo_lampada(String nome, String pass, String cod_dip);
+
+    @Query(value = " select dispositivo_tem_lampada(:cod_dip)", nativeQuery = true)
+    Iterable<String> controlo_tem_lampada(String cod_dip);
+
+    @Query(value = "select dispositivo.estado_portao from dispositivo Where rkay=:cod_dip", nativeQuery = true)
+    Iterable<String> estado_portao(String cod_dip);
+
+    @Query(value = "select dispositivo.estado_luz from dispositivo Where rkay=:cod_dip", nativeQuery = true)
+    Iterable<String> estado_luz(String cod_dip);
+
+
+
+
+    @Query(value = "select res_gar.garagens_id from reserva inner join utilizador on id_utilizador = utilizador_id left join res_gar on reserva.reservas_id = res_gar.reservas_id where utilizador_email = :nome and utilizador_pass =:pass)", nativeQuery = true)
+    Iterable<String> utilizador_reserva(String nome, String pass);
+
     @Query(value = "select criar_garagens(:utilizador_email,:utilizador_pass,:garagem_localizacao,:garagem_zona,:garagem_zona_cod,:garagem_lampada)", nativeQuery = true)
     Iterable<String> add_garagens(String utilizador_email,String utilizador_pass,String garagem_localizacao,String garagem_zona,int garagem_zona_cod,boolean garagem_lampada);
 
     @Query(value = "select utilizador_name from utilizador where utilizador_name = :nome and utilizador_morada = :morada and utilizador_gender = :genero and utilizador_bdate = :data and utilizador_identificacao_id = :identificacao and utilizador_email = :email and utilizador_pass =:pass", nativeQuery = true)
     Iterable<String> teste(String nome, String morada, String genero, Date data ,int identificacao,String email, String pass);
 
-    @Modifying
-    @Query(value = " select cria_utilizador(:nome,:morada,:genero,:data,:identificacao,:email,:pass", nativeQuery = true)
+    @Query(value = " select cria_utilizador(:nome,:morada,:genero,:data,:identificacao,:email,:pass)", nativeQuery = true)
     Iterable<String> criar_utilizador(String nome, String morada, String genero, Date data ,int identificacao,String email, String pass);
+
 
 }
