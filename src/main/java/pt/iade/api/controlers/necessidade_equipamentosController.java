de85b073dcd042a52d42e43ccf67e855e8bdd2c7
brunopreto <garage4uiade@gmail.com>
2023-07-05 18:56:56 +0100

    querys

--- src/main/java/pt/iade/api/controlers/necessidade_equipamentosController.java
+++ src/main/java/pt/iade/api/controlers/necessidade_equipamentosController.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import pt.iade.api.models.Exceptions.NotFoundException;
 import pt.iade.api.models.Exceptions.Response;
-import pt.iade.api.models.equipamentos;
+import pt.iade.api.models.necessidade_equipamentos;
 import pt.iade.api.models.repositories.Necessidade_equipamentosRepository;
 
 import java.util.Optional;
@@ -22,33 +22,33 @@ public class necessidade_equipamentosController {
     private Necessidade_equipamentosRepository necessidade_equipamentosRepository;
 
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Necessidade_equipamentos> getNecessidade_equipamentos() {
+    public Iterable<necessidade_equipamentos> getNecessidade_equipamentoss() {
         logger.info("Sending all necessidade_equipamentos");
         return necessidade_equipamentosRepository.findAll();
     }
 
     @GetMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public equipamentos getNecessidade_equipamentos(@PathVariable int id) {
+    public necessidade_equipamentos getNecessidade_equipamentos(@PathVariable int id) {
         logger.info("Sending necessidade_equipamentos with id " + id);
-        Optional<equipamentos> _necessidade_equipamentos = necessidade_equipamentosRepository.findById(id);
-        if (!_equipamentos.isPresent())
+        Optional<necessidade_equipamentos> _necessidade_equipamentos = necessidade_equipamentosRepository.findById(id);
+        if (!_necessidade_equipamentos.isPresent())
             throw new NotFoundException("" + id, "Necessidade_equipamentos", "id");
         else
-            return _equipamentos.get();
+            return _necessidade_equipamentos.get();
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public equipamentos saveEquipamentos(@RequestBody equipamentos equipamentos) {
-        pt.iade.api.models.equipamentos savedEquipamentos = equipamentosRepository.save(equipamentos);
-        logger.info("Saving equipamentos with id " + savedEquipamentos.getId());
-        return savedEquipamentos;
+    public necessidade_equipamentos saveNecessidade_equipamentos(@RequestBody necessidade_equipamentos necessidade_equipamentos) {
+        pt.iade.api.models.necessidade_equipamentos savedNecessidade_equipamentos = necessidade_equipamentosRepository.save(necessidade_equipamentos);
+        logger.info("Saving necessidade_equipamentos with id " + savedNecessidade_equipamentos.getId());
+        return savedNecessidade_equipamentos;
     }
 
     @DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response deleteEquipamentos(@PathVariable int id) {
-        logger.info("Deleting equipamentos with id " + id);
+    public Response deleteNecessidade_equipamentos(@PathVariable int id) {
+        logger.info("Deleting necessidade_equipamentos with id " + id);
         // No verification to see if it exists
-        equipamentosRepository.deleteById(id);
-        return new Response("Deleted equipamentos with id " + id, null);
+        necessidade_equipamentosRepository.deleteById(id);
+        return new Response("Deleted necessidade_equipamentos with id " + id, null);
     }
 }
